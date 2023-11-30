9604d54b8151a10a55777701160f04af906e9463
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-11-30 11:36:02 +0800

    Use destructors to prevent resource leaks

---
+++ Item 09.cpp
@@ -0,0 +1,9 @@
+/**
+ * Use destructors to prevent resource leaks.
+*/
+
+/**
+ * The principle of this item is valid, but the content is outdated.
+ *
+ * Use smart pointer in <memory> library.
+*/
