2f4ba0201fa2613c45fbe52740c72f7873ee15d1
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-12-01 09:08:34 +0800

    https://github.com/The-Lonesome-Coder/Effective-CPP

---
+++ Item 11.cpp
@@ -0,0 +1,8 @@
+/**
+ * Prevent exceptions from leaving destructors.
+*/
+
+/**
+ * This item is the same as Effective C++ (3rd Edition, 2005) - Item 08.
+ * See: https://github.com/The-Lonesome-Coder/Effective-CPP
+*/
