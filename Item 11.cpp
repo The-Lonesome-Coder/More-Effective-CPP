67599d95569a4d4904fd87e4c4d2897a75d6d5d6
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-12-05 15:14:07 +0800

    Prevent exceptions from leaving destructors

---
+++ Item 11.cpp
@@ -0,0 +1,8 @@
+/**
+ * Prevent exceptions from leaving destructors.
+*/
+
+/**
+ * The is a duplicate item from Effective C++ (3rd Edition, 2005) Item 08.
+ * See: https://github.com/The-Lonesome-Coder/Effective-CPP
+*/
