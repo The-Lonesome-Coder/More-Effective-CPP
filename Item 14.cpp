30d25abbf5754adf8a88d3833a115095f8b6e841
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-12-05 15:39:49 +0800

    Deprecated and Removed: Use exception specifications judiciously

---
+++ Item 14.cpp
@@ -0,0 +1,8 @@
+/**
+ * Use exception specifications judiciously.
+*/
+
+/**
+ * This Item is deprecated in C++11, and removed in C++17.
+ * Additionally, throw() specifier is deprecated in C++17, and removed in C++20.
+*/
